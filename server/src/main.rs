7fafaaf1b85d32b512da5da84ee46b76c7fb6ef1
LordBurtz <fridolin.karger@gmail.com>
2022-05-03 16:20:21 +0200

    messing around

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,17 @@
+mod todo_rest;
+
+use todo_rest::todos_filter;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let root = warp::path::end().map(|| "Welcome to my warp server");
-    let test = warp::path("shopping").map(|| "test site");
-    let routes = root.or(test).with(warp::cors().allow_any_origin());
+    let root = warp::path::end().map(|| "hello from le root");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
+    let hi = warp::path("hi").and(warp::get()).map(|| "hi");
+
+    let api = hi.or(todos_filter());
+
+    let routes = api.or(root).or(hi);
+
+    warp::serve(routes).run(([127,0,0,1], 5000)).await;
 }
