b7936e2c27d0cd0c05999deafe24eb5b72b0cc23
LordBurtz <fridolin.karger@gmail.com>
2022-05-03 13:37:46 +0200

    test done

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,8 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let root = warp::path::end().map(|| "Welcome to my warp server");
-    let routes = root.with(warp::cors().allow_any_origin());
+    let test = warp::path("shopping").map(|| "test site");
+    let routes = root.or(test).with(warp::cors().allow_any_origin());
 
     warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
 }
