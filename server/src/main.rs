2b4982933b7834c5610f6c165cc5826ffb60d26e
LordBurtz <fridolin.karger@gmail.com>
2022-05-07 08:52:02 +0200

    rocket done???

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,26 +3,41 @@ mod todo_rest;
 #[macro_use]
 extern crate rocket;
 
+use rocket::State;
+use rusqlite::{Connection};
+use todo_rest::SQLWrapper;
+use std::sync::{Arc, Mutex};
+
+struct TestState {
+    db: Mutex<SQLWrapper>,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello from root"
 }
 
-#[get("/hi")]
-fn hi() -> &'static str {
-    "Hello there"
+#[get("/encrstate/<hash>")]
+fn greeter(hash: String, db: &State<TestState>) -> String {
+    let res = db.db.lock().unwrap();
+    res.get_encr_state(hash).unwrap().name_hash
 }
 
-#[get("/greet/<name>")]
-fn greeter(name: String) -> String {
-    format!("Hello, {}!", name)
+#[get("/state/<user>")]
+fn get_state (user: String) -> String {
+    user
 }
 
 #[rocket::main]
 async fn main() {
+    let db = TestState {db: Mutex::new(SQLWrapper {
+        conn: Connection::open_in_memory().unwrap()
+    })};
+
     rocket::build()
+        .manage(db)
         .mount("/", routes![index])
-        .mount("/", routes![hi, greeter])
+        .mount("/", routes![greeter])
         .launch()
         .await;
 }
--- server/src/todo_rest.rs
+++ server/src/todo_rest.rs
@@ -1,24 +1,24 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result};
 
 #[derive(Debug)]
-struct EncrState {
-    name_hash: String,
+pub struct EncrState {
+    pub name_hash: String,
     iv: [isize; 16],
     salt: [isize; 9],
 }
 
 #[derive(Debug)]
-struct Entry {
+pub struct Entry {
     name_hash: String,
     blob: Vec<u8>,
 }
 
-struct SQLWrapper {
-    conn: Connection
+pub struct SQLWrapper {
+    pub conn: Connection
 }
 
 impl SQLWrapper {
-    pub fn get_encr_state(name_hash: String) -> Result<EncrState> {
+    pub fn get_encr_state(&self, name_hash: String) -> Result<EncrState> {
         //TODO: implement it lol
         Ok(EncrState {
             name_hash: "fridolin".to_owned(),
