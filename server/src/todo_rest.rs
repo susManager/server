203c2e2e6b56482cf1fb6f64deb8c31f1a005aaa
LordBurtz <fridolin.karger@gmail.com>
2022-05-11 20:15:44 +0200

    finanlly not crashing anymore

--- server/src/todo_rest.rs
+++ server/src/todo_rest.rs
@@ -22,7 +22,7 @@ impl SQLWrapper {
     pub fn get_encrstate(&self, name_hash: String) -> String {
         //TODO: implement it lol
  //"{\"dc7b5396c805c06f0ac60390322e4142a0bceb35\": {\"iv\": [-103, 37, -19, -3, 47, -121, -31, 79, 1, 54, 94, 56, -107, 100, -87, 104],\"salt\" : [1, 3, 3, 7, 4, 2, 0, 6, 9],\"algo\": \"AES/CBC/PKCS5Padding\"}}".to_string()
-        let mut stmt = self.conn.prepare(format!("SELECT  json from encrstate where name_hash == {}", name_hash).as_str()).unwrap();
+        let mut stmt = self.conn.prepare(format!("SELECT  json from encrstate where name_hash == \"{}\"", "fridolin").as_str()).unwrap();
         let mut name = "".to_string();
         let iter = stmt.query_map([], |row| {
             row.get(0)
@@ -47,6 +47,6 @@ pub fn est_database_conn() -> Connection {
     let conn = Connection::open_in_memory().expect("failed to establish connection..\n aborting..");
     conn.execute("CREATE TABLE IF NOT EXISTS encrstate (name_hash TEXT PRIMARY KEY, json TEXT);", []);
     conn.execute("CREATE TABLE IF NOT EXISTS entry (name_hash TEXT PRIMARY KEY, blob TEXT);", []);
-    conn.execute("INSERT into enrstate (name_hash, json) values (\"fridolin\", \"{allah}\"", []);
+    conn.execute("INSERT into enrstate (name_hash, json) values (\"fridolin\", \"{allah}\";", []);
     conn
 }
