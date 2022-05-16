37a296622c750b056324748d7c0cd6bc201be1f1
LordBurtz <fridolin.karger@gmail.com>
2022-05-16 15:44:47 +0200

    working request

--- server/src/todo_rest.rs
+++ server/src/todo_rest.rs
@@ -22,15 +22,18 @@ impl SQLWrapper {
     pub fn get_encrstate(&self, name_hash: String) -> String {
         //TODO: implement it lol
  //"{\"dc7b5396c805c06f0ac60390322e4142a0bceb35\": {\"iv\": [-103, 37, -19, -3, 47, -121, -31, 79, 1, 54, 94, 56, -107, 100, -87, 104],\"salt\" : [1, 3, 3, 7, 4, 2, 0, 6, 9],\"algo\": \"AES/CBC/PKCS5Padding\"}}".to_string()
-        let mut stmt = self.conn.prepare(format!("SELECT  json from encrstate where name_hash == \"{}\"", "fridolin").as_str()).unwrap();
-        let mut name = "".to_string();
+        let mut stmt = self.conn.prepare(format!("SELECT  json from encrstate where name_hash == \"{}\"", name_hash).as_str()).unwrap();
+        let mut result = "".to_string();
         let iter = stmt.query_map([], |row| {
-            row.get(0)
+            Ok(EncrState {
+                name_hash: name_hash.to_string(),
+                json: row.get(0).unwrap()
+            })
         }).unwrap();
         for r in iter {
-            name = r.unwrap();
+            result.push_str(r.unwrap().json.as_str());
         }
-        name
+        result
     }
 
     pub fn get_entry(name_hash: String) -> Result<Entry> {
@@ -44,9 +47,10 @@ impl SQLWrapper {
 
 pub fn est_database_conn() -> Connection {
     //TODO: this has to be a file, will be implemented later
-    let conn = Connection::open_in_memory().expect("failed to establish connection..\n aborting..");
+    let conn = Connection::open("./test-db.db3").expect("failed to establish connection..\n aborting..");
     conn.execute("CREATE TABLE IF NOT EXISTS encrstate (name_hash TEXT PRIMARY KEY, json TEXT);", []);
     conn.execute("CREATE TABLE IF NOT EXISTS entry (name_hash TEXT PRIMARY KEY, blob TEXT);", []);
-    conn.execute("INSERT into enrstate (name_hash, json) values (\"fridolin\", \"{allah}\";", []);
+    conn.execute("INSERT into encrstate (name_hash, json) values (\"fridolin\", \"{allah}\");", []);
+    conn.flush_prepared_statement_cache();
     conn
 }
